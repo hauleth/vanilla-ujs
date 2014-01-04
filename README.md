23bab33141424fa4ee695edac476595c589ce90d
Łukasz Niemier <lukasz@niemier.pl>
2014-01-04 20:22:05 +0100

    Write README.md

---
+++ README.md
@@ -0,0 +1,25 @@
+# Vanilla UJS
+
+It is implementation of Rails [jQuery UJS][jq-ujs] in pure JavaScript.
+No extra dependencies.
+
+## Install
+
+Use [Bower][bower]:
+
+    bower install vanilla-ujs
+
+## Contribute
+
+1. Clone repo
+
+      $ git clone git://github.com/hauleth/vanilla-js.git
+      $ cd vanilla-js/
+
+2. Install dependencies
+
+      $ npm install
+
+3. Run tests
+
+      $ grunt test
---
+++ vanilla-ujs.js
@@ -0,0 +1,42 @@
+(function (window, document, undefined) {
+var matches = (function(doc) {
+  return doc.matchesSelector ||
+    doc.webkitMatchesSelector ||
+    doc.mozMatchesSelector ||
+    doc.oMatchesSelector ||
+    doc.msMatchesSelector;
+})(document.documentElement);
+
+document.addEventListener('click', function(e) {
+  if ( matches.call( e.target, 'ul a') ) {
+    var  form, input, method;
+
+    if (matches(element, 'a[data-remote]')) {
+      return;
+    }
+
+    method = element.getAttribute('data-method').toLowerCase();
+    if (method == 'get') {
+      return;
+    }
+
+    form = document.createElement('form');
+    form.method = 'POST';
+    form.action = element.attr('href');
+    form.style.display = 'none';
+
+    if (method != 'post') {
+      input = document.createElement('input');
+      input.setAttribute('type', 'hidden');
+      input.setAttribute('name', '_method');
+      input.setAttribute('value', method);
+      form.appendChild(input);
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+    event.preventDefault();
+    return false;
+  }
+}, false);
+}).call(this, window, document);
