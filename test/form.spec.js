3fd91248406b1f0e9ebbed8adfe679bbe9c1b1d4
miks <miks@cubesystems.lv>
2016-07-18 15:20:57 +0200

    Remove unnecessary whitespaces

--- test/form.spec.js
+++ test/form.spec.js
@@ -11,21 +11,21 @@ describe('Form methods', function () {
     form.appendChild(submit)
     win().submitForm = submitForm = sinon.spy();
   });
-  
+
   describe('[method=post]', function () {
     beforeEach(function () {
       form.onsubmit = submitForm;
       form.setAttribute('method', 'post');
     });
-    
+
     describe('no [data-remote]', function () {
       beforeEach(function () {
         form.setAttribute('action', '/echo?callback=parse');
       });
-  
+
       it('is submitted as a form', function (done) {
         var url = win().location.href;
-  
+
         window.parse = function (json) {
           expect(url).to.not.equal(win().location.href);
           expect(json).to.deep.equal({
@@ -34,41 +34,41 @@ describe('Form methods', function () {
           });
           done();
         };
-        
+
         click(submit);
-        
+
         expect(submitForm.called).to.be.true;
       });
-      
+
     });
-  
+
     describe('[data-remote]', function () {
       beforeEach(function () {
         form.setAttribute('action', '/xhr');
         form.setAttribute('data-remote', 'true');
       });
-      
+
       it('is sent as XHR request', function (done) {
         var url = win().location.href;
-        
+
         var handler = function (event) {
           expect(url).to.equal(win().location.href);
           expect(JSON.parse(event.detail.response)).to.deep.equal({
             method: 'post',
             path: '/xhr'
           });
-          
+
           doc().removeEventListener('ajax:complete', handler);
           done();
         };
-        
+
         doc().addEventListener('ajax:complete', handler);
-        
+
         click(submit);
-        
+
         expect(submitForm.called).to.be.true;
       });
     });
   });
-  
+
 });
